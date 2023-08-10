56fdff1410dda71795150d905eea5642b1d9cc0a
Ivan Leo <ivanleomk@gmail.com>
2023-08-10 16:37:50 +0800

    Fixed up a new Clerk.dev integration

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div><p>Dashboard</p><Link href="/"> Go Back Home</Link></div>
+  )
+}
+
+export default Dashboard
