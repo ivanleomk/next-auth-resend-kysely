3307af384a7e726f0d88bb8bdcf20f2e1d06e692
Ivan Leo <ivanleomk@gmail.com>
2023-08-14 16:52:23 +0800

    Remove hardcoded values and touch up imports

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,7 +3,7 @@ import { SiteConfig } from "@/types";
 export const siteConfig: SiteConfig = {
   name: "Brain Dump",
   description:
-    "A simple note taking app built with Next.js, Tailwind CSS, and Clerk.",
+    "Capture everything and organize it later",
   url: "https://notes.ivanleo.com",
   links: {
     twitter: "https://twitter.com/ivanleomk",
--- src/lib/user.ts
+++
@@ -1,6 +0,0 @@
-"use server"
-import { db } from "./db";
-
-export const getUserDetails = async () => {
-  return null
-}
