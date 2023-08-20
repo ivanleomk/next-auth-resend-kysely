7df591a677f4156edc9a042d553c1283a08e740e
Ivan Leo <ivanleomk@gmail.com>
2023-08-20 14:17:10 +0800

    Removed the use of the smtp server env variables

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -12,16 +12,6 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 
 const providerConfig = EmailProvider({
-  server: {
-    host: process.env.EMAIL_SERVER_HOST,
-    port: process.env.EMAIL_SERVER_PORT,
-    auth: {
-      user: process.env.EMAIL_SERVER_USER,
-      pass: process.env.RESEND_API_KEY,
-    },
-  },
-  from: process.env.EMAIL_FROM,
-
   sendVerificationRequest: async ({ identifier, url, provider }) => {
     try {
       const isDevOrStaging = process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview"
